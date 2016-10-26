b3e38246ebc1524314983da83c600f0a0852a1be
Theodore Zilist <ted@carlabs.com>
2016-10-26 16:54:04 -0700

    refactor logger a bit

--- src/config/winston.js
+++ src/config/winston.js
@@ -1,6 +1,15 @@
 import winston from 'winston';
 import chalk from 'chalk';
 
+function defineMeta(options, color) {
+  if (options.level === 'error') {
+    return `\n\n${chalk[color].black('STACK TRACE')}\n${JSON.stringify(options.meta.stack, undefined, 1)}`;
+  } else if (options.meta && Object.keys(options.meta).length) {
+    return `\n\n${chalk[color].black('META DATA')}\n${JSON.stringify(options.meta, undefined, 1)}`;
+  }
+  return '';
+}
+
 const logger = new winston.Logger({
   level: 'info',
   transports: [
@@ -8,21 +17,11 @@ const logger = new winston.Logger({
       level: 'debug',
       prettyPrint: true,
       handleExceptions: true,
-      timestamp() {
-        return Date.now();
-      },
       formatter(options) {
-        // Return string will be passed to logger.
         const color = options.level === 'error' ? 'bgRed' : 'bgGreen';
-        let meta;
-        if (options.level === 'error') {
-          meta = `\n\n${chalk[color].black('STACK TRACE')}\n${JSON.stringify(options.meta.stack, undefined, 1)}`;
-        } else if (options.meta && Object.keys(options.meta).length) {
-          meta = `\n\n${chalk[color].black('META DATA')}\n${JSON.stringify(options.meta, undefined, 1)}`;
-        } else {
-          meta = '';
-        }
-        return `${chalk.bgBlue.white(new Date())}\n\n${chalk[color].black(options.level.toUpperCase())}   ${chalk.cyan(options.message || '')} ${meta}`;
+        const meta = defineMeta(options, color);
+        // Return string will be passed to logger.
+        return `${chalk.bgBlue.white(new Date())}\n\n${chalk[color].black(options.level.toUpperCase())} - ${chalk.cyan(options.message || '')} ${meta}`;
       },
     }),
     new (winston.transports.File)({
