dfc719dc1dca2f3eb6357a25a9d9fc801946919e
Ray Putt <rpputt@hotmail.com>
2016-10-30 14:22:51 -0700

    made it so the referral links open in a new tab

--- src/public/components/Deal.jsx
+++ src/public/components/Deal.jsx
@@ -5,7 +5,7 @@ var Deal = React.createClass({
     render: function() {
 
         return (
-            <a href={this.props.targetUrl}>
+            <a href={this.props.targetUrl} target='_blank'>
                 <div className='Deal' id={this.props.key}>
                     <h2>{this.props.dealTitle}</h2>
                     <h3>{this.props.merchant}</h3>
